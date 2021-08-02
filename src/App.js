54a9fdb931767601de6df90e5d491b2a6e8bcaca
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-08-02 13:46:58 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+  const[people,setPeople]=useState(data)
+  return <main>
+    <section className="container">
+      <h3>0 birthdays today</h3>
+      <List />
+      <button onClick={()=>console.log('you clicked me')}>clear all</button>
+    </section>
+    </main>;
+}
+
+export default App;
